cc5e0fd91e3574b6720237e37d77a9f18380a83e
wuyuwei <wuyuwei2016@gmail.com>
2021-08-03 15:19:56 +0800

    feat:自动打出types文件

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,40 @@
 /* prod 配置 */
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from "rollup-plugin-typescript2";
+import { nodeResolve } from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import typescript from "rollup-plugin-typescript2"
+import { terser } from "rollup-plugin-terser"
 
 export default {
-    input: 'src/index.ts',
-    output: [
-      {
-        file: './lib/bundle.js',
-        format: 'cjs',
-        banner:injectDateForBuild,
-      },
-      {
-        file: './esm/bundle.js',
-        format: 'es',
-        banner: injectDateForBuild
-      },
-    ],
-    plugins: [
-      nodeResolve(),
-      commonjs(),
-      typescript({
-        tsconfig: "tsconfig.json",
-        useTsconfigDeclarationDir: true,
+  input: "src/index.ts",
+  output: [
+    {
+      file: "./lib/bundle.js",
+      format: "cjs",
+      banner: injectDateForBuild
+    },
+    {
+      file: "./esm/bundle.js",
+      format: "es",
+      banner: injectDateForBuild
+    }
+  ],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    terser(),
+    typescript({
+      tsconfig: "tsconfig.json",
+      useTsconfigDeclarationDir: true
     })
-    ],
-};
+  ]
+}
 
- 
-function injectDateForBuild(){
-  return new Promise(resolve => {
-    const v = new Date();
-    const buildDate =  `${v.getFullYear()}-${v.getMonth() + 1}-${v.getDate()} ${v.getHours()}:${v.getMinutes()}:${v.getSeconds()}`;
-    resolve("/* You version is:" + 'pkg-version' + " " + "Build Date:" + buildDate + "*/")
+function injectDateForBuild() {
+  return new Promise((resolve) => {
+    const v = new Date()
+    const buildDate = `${v.getFullYear()}-${
+      v.getMonth() + 1
+    }-${v.getDate()} ${v.getHours()}:${v.getMinutes()}:${v.getSeconds()}`
+    resolve("/* You version is:" + "pkg-version" + " " + "Build Date:" + buildDate + "*/")
   })
 }
